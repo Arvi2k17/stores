570a1ee9350136167df9d598ff82d315be4c7998
Aravinthvenkatram G <aravinthvenkatram.g@bizmetric.com>
2021-04-30 22:23:28 +0530

    server running successfully

--- apps/pg-server/src/main.ts
+++ apps/pg-server/src/main.ts
@@ -3,3 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
+import  {AppServer} from './server'
+
+const app = new AppServer().getApp();
+export { app };
--- apps/pg-server/src/server.ts
+++ apps/pg-server/src/server.ts
@@ -1,19 +1,59 @@
 import * as express from 'express'
 import {Server,createServer} from 'http'
-
+import * as colors from 'colors';
 
 export class AppServer {
 	private readonly PORT = process.env.PORT || 25001;
 
-	private app = express.application;
+	private app : express.Application;
 
-	private server = Server;
+	private server : Server;
 
 	constructor() {
+		this.initServer()
+	}
 
+	private async initServer(){
+		this.CreateServer()
+		await this.addRoutes();
+		// await this.dbSetup();
+
+		this.listen()
 	}
 
 	private CreateServer() {
-		const cors =require('cors')
+		const cors =require('cors');
+		const fileUpload = require('express-fileupload');
+
+		this.app = express();
+		
+		this.app.use(cors());
+		this.app.options('*', cors());
+
+		this.app.use(
+			fileUpload({
+				createParentPath:true
+			})
+		)
+
+		this.server = createServer(this.app)
 	}
+
+	private async addRoutes() {
+		// const startupCtrl: StartupController = new StartupController();
+	
+		this.app.get('/', (req, res) => {
+		  res.send({ message: `Welcome to Service Middleware App!` });
+		});
+	}
+
+	private listen (){
+		this.server.listen(this.PORT, () => {
+			console.log(colors.green(`Server listening on http://localhost:${this.PORT}`));
+		  });
+	}
+
+	public getApp(): express.Application {
+		return this.app;
+	  }
 }
